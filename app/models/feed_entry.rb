5445849f8318497cfc13c0bcba6d3344da62c4d7
Brian Keim <westxbk@gmail.com>
2016-05-20 23:16:34 -0400

    Added real estate rss feed

--- app/models/feed_entry.rb
+++ app/models/feed_entry.rb
@@ -13,4 +13,22 @@ class FeedEntry < ActiveRecord::Base
       end
     end
   end
+
+  def self.realEstateFeed
+    url = "http://www.capitalanalyticsassociates.com/category-real-estate/feed/"
+    feed_url = url
+    feed = Feedjira::Feed.fetch_and_parse(feed_url)
+    feed.entries.each do |entry|
+      unless exists? :guid => entry.id
+        create!(
+        :name           => entry.title,
+        :summary        => entry.summary,
+        :url            => entry.url,
+        :published_at   => entry.published,
+        :guid           => entry.id,
+        )
+      end
+    end
+  end
+
 end
