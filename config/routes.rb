af48dadd40bf15a0363ff482bcf021c8ab2904bb
Brian Keim <westxbk@gmail.com>
2016-05-21 18:00:25 -0400

    added stripe and contact us section

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :enterprises
   resources :industries
   resources :models
+  resources :charges
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
